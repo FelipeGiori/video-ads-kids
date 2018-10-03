4071614678d20a2825d423b17d51af686c44b322
Guilherme Soares <guilhermesoares@dcc.ufmg.br>
2018-10-03 14:06:16 -0300

    Added persona 7 docker

--- start_bot.py
+++ start_bot.py
@@ -15,12 +15,11 @@ def get_time():
     mg_times = ["9:00", "15:00", "19:00"]
     pb_times = ["11:00", "17:00", "21:00"]
 
-    # TODO: Adicionar docker da persona 7
-    # TODO: Setup da vpn para Sao Paulo
     docker = ["d6f18b0f8e45bebf19f2de53c469cebc429d2f013b896ba3b1f474e9bd43b271",
              "3a7bb8ba7918c766debf1571924f8eb056938606dfb8cd63d05505b7a761772f",
              "3ee141a9362caaaaf0ebaedc2ba3734e2f11fdf385ad8bea1da59aa8e5c59a67",
-             "825c4b38ff7c96db7686846cd172ec2a2b566960c7d3627e38ecd2330d0de2ce"]
+             "825c4b38ff7c96db7686846cd172ec2a2b566960c7d3627e38ecd2330d0de2ce",
+             "0a09016f0d144dcbb1c0891bc80a99ef56d82087a8f4e825a8d68382b092b98d"]
 
     if(docker_id == docker[0]):
         return pb_times
@@ -29,6 +28,8 @@ def get_time():
     elif(docker_id == docker[2]):
         return mg_times
     elif(docker_id == docker[3]):
+        return mg_times
+    elif(docker_id == docker[4]):
         return mg_times
     else:
         print("Docker id não existe")
